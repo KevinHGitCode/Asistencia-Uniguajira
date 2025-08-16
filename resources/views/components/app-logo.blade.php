7344f694df8d70f4f961f1bafe8062f80c79662b
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-08-16 18:23:19 -0500

    - correccion del tamaño del logo en sidebar - se elimino la vista welcome

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,12 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+<div class="flex aspect-square size-10 items-center justify-center rounded-md overflow-hidden">
+    <img 
+        src="{{ asset('images/logo-uniguajira-seo-150x150.webp') }}" 
+        alt="Logo Uniguajira" 
+        class="object-contain w-full h-full"
+    >
 </div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+<div class="grid flex-1 text-start text-sm">
+    <span class="mb-0.5 truncate leading-tight font-semibold">
+        {{ config('app.name', 'Laravel') }}
+    </span>
 </div>
