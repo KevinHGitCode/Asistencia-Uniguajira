05ba2cb700bd01d2f683eb60155924f16ff64123
Danie1l6 <danielsierra103@gmail.com>
2025-08-16 21:17:22 -0500

    Añadir script y estilos para CalHeatmap; implementar calendario en el dashboard

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,6 +6,9 @@
 <link rel="icon" href="{{ asset('images/favicon-uniguajira-32x32.webp') }}" sizes="any">
 <link rel="icon" href="{{ asset('images/favicon-uniguajira-32x32.webp') }}" type="image/svg+xml">
 <link rel="apple-touch-icon" href="{{ asset('images/favicon-uniguajira-32x32.webp') }}">
+<script src="https://d3js.org/d3.v6.min.js"></script>
+<script src="https://unpkg.com/cal-heatmap/dist/cal-heatmap.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/cal-heatmap/dist/cal-heatmap.css">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
