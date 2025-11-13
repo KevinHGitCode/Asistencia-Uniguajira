f3766d99577c9cb3d9fc69ed5907e3c9ab2fd6b6
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2025-11-13 15:46:41 -0500

    Correciones menores

--- resources/js/charts/general/index.js
+++ resources/js/charts/general/index.js
@@ -7,12 +7,11 @@ import { renderTopParticipants } from './topParticipants.js';
 import { renderTopUsers } from './topUsers.js';
 import { renderEventsByRole } from './eventsByRole.js';
 import { renderEventsByUser } from './eventsByUser.js';
-import { renderEventsOverTime } from './eventsOverTime.js';
-import { renderAttendancesOverTime } from './attendancesOverTime.js';
+// import { renderEventsOverTime } from './eventsOverTime.js';
+// import { renderAttendancesOverTime } from './attendancesOverTime.js';
 import { initStatisticsCounters, cleanupStatisticsCounters } from './statisticsCounters.js';
 import { updateFilters } from './filtersManager.js';
 
-// Objeto para mantener las referencias de los gráficos
 const charts = {
   programAttendancesBar: null,
   programParticipantsPie: null,
@@ -25,9 +24,6 @@ const charts = {
   eventsByUser: null
 };
 
-/**
- * Renderiza todos los gráficos
- */
 function renderAllCharts() {
   renderProgramAttendancesBar(charts);
   renderProgramParticipantsPie(charts);
@@ -40,64 +36,39 @@ function renderAllCharts() {
   renderEventsByUser(charts);
 }
 
-/**
- * Inicializa el sistema de gráficos
- */
 function initChartsSystem() {
-  // Inicializar listeners globales (resize, theme, cleanup)
   chartsManager.initGlobalListeners();
-
-  // Inicializar contadores de estadísticas
   initStatisticsCounters();
-
-  // Renderizar todos los gráficos
   renderAllCharts();
-
   console.log('✓ Sistema de gráficos inicializado');
 }
 
-/**
- * Limpia el sistema de gráficos
- */
 function cleanupChartsSystem() {
-  // Limpiar contadores
   cleanupStatisticsCounters();
-
-  // Limpiar manager de gráficos
   chartsManager.cleanup();
-
   console.log('✓ Sistema de gráficos limpiado');
 }
 
-/**
- * Función principal exportada globalmente
- */
 window.createGeneralCharts = () => {
-  // Limpiar sistema anterior si existe
   cleanupChartsSystem();
 
-  // Inicializar nuevo sistema
-  initChartsSystem();
+  // Esperar un tick para asegurar que el DOM está actualizado
+  requestAnimationFrame(() => {
+    initChartsSystem();
+  });
 
-  // Escuchar cambios de filtros
   window.addEventListener('statistics-filters-changed', (event) => {
     const { filters } = event.detail;
     updateFilters(filters);
-
-    // Recargar todos los gráficos con los nuevos filtros
     renderAllCharts();
-
-    // Recargar contadores
     initStatisticsCounters();
   });
 
-  // Escuchar cambios de tema para recargar gráficos
   window.addEventListener('charts-theme-changed', () => {
     renderAllCharts();
   });
 };
 
-// Exponer función de cleanup globalmente
 window.cleanupGeneralCharts = cleanupChartsSystem;
 
 console.log('✓ Módulo charts/general cargado');
