b9698d3f79f6b68a9d1c6f8d92effec322755772
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2026-03-09 21:08:56 -0500

    add: mejoras en los seeder de evento para tener un evento en proceso al ejecutar

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Dependency;
 use App\Models\Area;
 use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class EventSeeder extends Seeder
 {
@@ -19,6 +20,7 @@ class EventSeeder extends Seeder
 
         $userIds = User::pluck('id')->toArray();
         $dependencies = Dependency::with('areas')->get();
+        $now = now();
 
         $rows = [];
 
@@ -32,14 +34,22 @@ class EventSeeder extends Seeder
             $areaId = null;
 
             if ($dependency) {
-                // 50% probabilidad de asignar área si la dependencia tiene áreas
+                // 50% probabilidad de asignar area si la dependencia tiene areas
                 if ($dependency->areas->isNotEmpty() && $faker->boolean(50)) {
                     $areaId = $dependency->areas->random()->id;
                 }
             }
 
-            $start = $faker->time('H:i:s');
-            $end = $faker->time('H:i:s');
+            [$date, $start, $end] = match ($i) {
+                0 => $this->buildTimedEventWindow($now->copy()->subMinutes(5), 60),
+                1 => $this->buildTimedEventWindow($now->copy()->addMinutes(15), 60),
+                2 => $this->buildTimedEventWindow($now->copy()->addHour(), 60),
+                default => [
+                    $faker->dateTimeBetween('-6 months', '+3 months')->format('Y-m-d'),
+                    $faker->time('H:i:s'),
+                    $faker->time('H:i:s'),
+                ],
+            };
 
             $title = $faker->sentence(3);
             $slug = Str::slug($title) . '-' . uniqid();
@@ -49,7 +59,7 @@ class EventSeeder extends Seeder
                 'dependency_id' => $dependency?->id,
                 'area_id' => $areaId,
                 'description' => $faker->paragraph(),
-                'date' => $faker->dateTimeBetween('-6 months', '+3 months')->format('Y-m-d'),
+                'date' => $date,
                 'start_time' => $start,
                 'end_time' => $end,
                 'location' => $faker->city(),
@@ -61,5 +71,16 @@ class EventSeeder extends Seeder
         }
 
         Event::insert($rows);
+    }
+
+    private function buildTimedEventWindow(Carbon $startAt, int $durationMinutes): array
+    {
+        $endAt = $startAt->copy()->addMinutes($durationMinutes);
+
+        return [
+            $startAt->format('Y-m-d'),
+            $startAt->format('H:i:s'),
+            $endAt->format('H:i:s'),
+        ];
     }
 }
