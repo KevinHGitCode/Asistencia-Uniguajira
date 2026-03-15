15b223ffb6b36dd25699735d8c961b3a51d52568
Danie1l6 <danielsierra103@gmail.com>
2026-03-15 17:46:15 -0500

    comentar seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -81,8 +81,8 @@ class DatabaseSeeder extends Seeder
         $this->call(AreasSeeder::class);
         // $this->call(EventSeeder::class);
         $this->call(ProgramSeeder::class);
-        $this->call(AffiliationSeeder::class);
-        $this->call(ParticipantSeeder::class);
+        // $this->call(AffiliationSeeder::class);
+        // $this->call(ParticipantSeeder::class);
         // $this->call(AttendanceSeeder::class);
         $this->call(FormatSeeder::class);
     }
